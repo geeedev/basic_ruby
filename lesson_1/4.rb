cb9343b2bb6af89d17b01885d940571e487a093b
corpse <ge.upcase@gmail.com>
2018-08-03 11:09:32 +0300

    ///

--- lesson_1/4.rb
+++ lesson_1/4.rb
@@ -1,15 +1,17 @@
 print "Enter a :"
-a = gets.to_f
+a = gets.to_i
 print "Enter b :"
-b = gets.to_f
+b = gets.to_i
 print "Enter c :"
-c = gets.to_f
+c = gets.to_i
 
-d = b ** 2 - 4 * a * c
+d = b**2 - 4 * a * c
+
+puts d
 
 if d > 0
-  x_1 = -b + Math.sqrt(d) / 2 * a
-  x_2 = -b - Math.sqrt(d) / 2 * a
+  x_1 = (-b + Math.sqrt(d)) / (2 * a)
+  x_2 = (-b - Math.sqrt(d)) / (2 * a)
   puts "D #{d}; #{x_1}, #{x_2}"
 elsif d == 0
   x = - (b / 2 * a)
