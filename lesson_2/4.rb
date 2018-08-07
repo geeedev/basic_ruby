68d4cc0ae84489c9bbe5cea36977cbdd306a7f04
corpse <ge.upcase@gmail.com>
2018-08-08 00:30:11 +0300

    changed 4.rb, added each_with_index method

--- lesson_2/4.rb
+++ lesson_2/4.rb
@@ -1,18 +1,3 @@
-hash = {
-  a: true, b: false, c: false, d: false, e: true,
-  f: false, g: false, h: false, i: true, j: false,
-  k: false, l: false, m: false, n: false, o: true,
-  p: false, q: false, r: false, s: false, t: false,
-  u: true, v: false, w: false, x: false, y: true,
-  z: false
-}
+arr = ["a", "e", "o", "u", "i"]
 
-result = {}
-
-0.upto(21) do |i|
-  if hash.to_a[i][1] == true
-    result[hash.to_a[i][0]] = i + 1
-  end
-end
-
-puts result
+("a".."z").each_with_index {|char, index| puts "#{char} - #{index + 1}" if (arr).include?(char)}
